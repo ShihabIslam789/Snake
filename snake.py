ea0a6136306e16235c9dcc65360161ab9cd81d0e
Shihab Islam <Shihab.Islam90@myhunter.cuny.edu>
2022-07-27 13:22:03 -0400

    Starting code functions

---
+++ snake.py
@@ -0,0 +1,26 @@
+import pygame
+import random 
+import math
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+rows = 0
+w =0
+
+def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+	pass
+
+def move(self,dirnx,dirny):
+	pass
+
+def draw(self,surface,eyes=False):
+	pass
+
+class snake(object):
+	def __init__(self, color, pos):
+        pass
+
+
+def main():
+	pass
